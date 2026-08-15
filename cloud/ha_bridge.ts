afa856499e51966e8a1487bca92c72b29ed356a8
asata <asatan@gmail.com>
2026-08-15 14:01:38 +0900

    feat: Add support for Korean LG Refrigerator (2REFO1DBN3K_U) & Fix 68-byte hardcoding

--- cloud/ha_bridge.ts
+++ cloud/ha_bridge.ts
@@ -42,6 +42,7 @@ const t2deviceTypes: Record<string, T2Factory> = {
     ['2RES1VE61NFA2']: Dev_2RES1VE61NFA2,
     ['2REB1GLVB1__2']: Dev_2REB1GLVB1__2,
     ['2RES1VE600FWC']: Dev_2RES1VE600FWC,
+    ['2REFO1DBN3K_U']: Dev_2REF11EIDA__4,
     ['Y_V8_Y___W.B32QEUK']: Y_V8_Y___W_B32QEUK,
     ['F_V7_Y___W.B_2QEUK']: F_V8_Y___W_B_2QEUK, // NOTE: we reuse F_V8_Y___W_B_2QEUK as the models appear to be compatible
     ['F_V8_Y___W.B_2QEUK']: F_V8_Y___W_B_2QEUK,
